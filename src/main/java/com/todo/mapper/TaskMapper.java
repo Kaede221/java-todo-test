09ddfab39d55199a07b3678a5713353dab878bb1
Kaede221 <kaedeshimizu@qq.com>
2026-03-14 10:43:50 +0800

    feat: 实现了增删改查

--- src/main/java/com/todo/mapper/TaskMapper.java
+++ src/main/java/com/todo/mapper/TaskMapper.java
@@ -1,22 +1,23 @@
 package com.todo.mapper;
 
 import com.todo.pojo.Task;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 public interface TaskMapper {
     @Select("select * from all_tasks where id = #{id}")
-    Task getTaskById(int id);
+    Task getTaskById(@Param("id") int id);
 
     @Select("SELECT * from all_tasks")
     List<Task> getAllTasks();
 
     @Delete("DELETE from all_tasks where id = #{id}")
-    void deleleTaskById(int id);
+    void deleleTaskById(@Param("id") int id);
 
-    @Insert("INSERT INTO all_tasks (title, content, done) values (#{title}, #{content}, #{done})")
-    void addNewTask(String title, String content, boolean done);
+    @Insert("INSERT INTO all_tasks (title, content, done) values (#{title}, #{content}, false)")
+    void addNewTask(@Param("title") String title, @Param("content") String content);
+
+    @Update("UPDATE all_tasks SET title = #{title}, content = #{content}, done = #{done} WHERE id = #{id}")
+    void editTaskById(@Param("id") int id, @Param("title") String title, @Param("content") String content, @Param("done") boolean done);
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -16,4 +16,56 @@ public class MyTest {
         List<Task> tasks = taskMapper.getAllTasks();
         System.out.println(tasks);
     }
+
+    @Test
+    public void testGetTaskById() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        TaskMapper taskMapper = context.getBean("taskMapper", TaskMapper.class);
+
+        Task task = taskMapper.getTaskById(1);
+        System.out.println(task);
+    }
+
+    @Test
+    public void testAddTask() {
+        String title = "New Task Title";
+        String content = "New Task Content";
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        TaskMapper taskMapper = context.getBean("taskMapper", TaskMapper.class);
+
+        taskMapper.addNewTask(title, content);
+        System.out.println("添加成功");
+
+        List<Task> tasks = taskMapper.getAllTasks();
+        System.out.println(tasks);
+    }
+
+    @Test
+    public void testDeleteTaskById() {
+        int id = 3;
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        TaskMapper taskMapper = context.getBean("taskMapper", TaskMapper.class);
+
+        taskMapper.deleleTaskById(id);
+        System.out.println("删除成功");
+
+        List<Task> tasks = taskMapper.getAllTasks();
+        System.out.println(tasks);
+    }
+
+    @Test
+    public void testEditTaskById() {
+        int id = 1;
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        TaskMapper taskMapper = context.getBean("taskMapper", TaskMapper.class);
+
+        taskMapper.editTaskById(id, "New Title 2", "New Content 2", false);
+        System.out.println("修改成功");
+
+        List<Task> tasks = taskMapper.getAllTasks();
+        System.out.println(tasks);
+    }
 }
